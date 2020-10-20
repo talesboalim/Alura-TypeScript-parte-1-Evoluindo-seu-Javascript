d56ca4ed23389e8e3d72d1bfeab8402aedb71de1
talesboalim <talesboalim@gmail.com>
2020-10-19 22:56:44 -0300

    Incluída classe abstrata na view, incluído JQuery

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,20 +1,16 @@
-class View<T> {
+abstract class View<T> {
 
-    private _elemento: Element;
+    private _elemento: JQuery;
 
     constructor(seletor: string) {
 
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
 
     update(model: T) {
 
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
 
-    template(model: T): string {
-
-        throw new Error('Você deve implementar o método template');
-    }
-
+    abstract template(model: T): string;
 }
